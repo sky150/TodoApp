0748fc1e4cc92ae94df30152a18b2638e947b29a
Tareq9000 <tkattit@gmail.com>
2020-11-11 18:26:35 +0100

    Adding a nav and the different states of the todos.

--- app/src/main/java/com/example/todoapp/Done.java
+++ app/src/main/java/com/example/todoapp/Done.java
@@ -6,51 +6,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class TodosMain extends AppCompatActivity {
+public class Done extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_todos_main);
-
+        setContentView(R.layout.activity_main);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.all:
-                        Intent intent1 = new Intent(TodosMain.this, TodosMain.class);
+                        Intent intent1 = new Intent(Done.this, TodosMain.class);
                         startActivity(intent1);
-                        Toast.makeText(TodosMain.this, "All", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Done.this, "All", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.in_progress:
-                        Intent intent2 = new Intent(TodosMain.this, In_Progress.class);
+                        Intent intent2 = new Intent(Done.this, In_Progress.class);
                         startActivity(intent2);
-                        Toast.makeText(TodosMain.this, "In Progress", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Done.this, "In Progress", Toast.LENGTH_SHORT).show();
                         break;
                     case R.id.done:
-                        Intent intent3 = new Intent(TodosMain.this, Done.class);
+                        Intent intent3 = new Intent(Done.this, Done.class);
                         startActivity(intent3);
-                        Toast.makeText(TodosMain.this, "Done", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Done.this, "Done", Toast.LENGTH_SHORT).show();
                         break;
                 }
                 return true;
             }
         });
-    }
-
-    public void addButtonClicked(){
-        Intent intent = new Intent(this, AddTodo.class);
-        startActivity(intent);
-    }
-
-    public void addButtonClicked(View view) {
-        Intent intent = new Intent(this, AddTodo.class);
-        startActivity(intent);
     }
 }
