7caa41665301c8e42d92fee5406b2d8c92456860
sumbon <sumbon@bzz.ch>
2020-11-11 17:23:40 +0100

    Navigation

--- app/src/main/java/com/example/todoapp/MainActivity.java
+++ app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.todoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,45 +15,35 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+public class MainActivity extends AppCompatActivity {    @Override
+protected void onCreate(Bundle savedInstanceState) {
+    Intent intent;
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+    bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.all:
+                    Intent intent1 = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent1);
+                    Toast.makeText(MainActivity.this, "All", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.in_progress:
+                    Intent intent2 = new Intent(MainActivity.this, In_Progress.class);
+                    startActivity(intent2);
+                    Toast.makeText(MainActivity.this, "In Progress", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.done:
+                    Intent intent3 = new Intent(MainActivity.this, Done.class);
+                    startActivity(intent3);
+                    Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                    break;                }
             return true;
         }
-
-        return super.onOptionsItemSelected(item);
-    }
+    });
+}
 }
